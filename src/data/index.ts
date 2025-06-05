99ea1771194cd3d4a1eb0e64408ce38d24433c2a
Mohamed <moussabouremani@gmail.com>
2025-06-05 20:29:23 +0200

    ajou du fichier phto

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,8 @@
 import { Project, Skill, Education } from '../types';
 import imgJeuVideo from '../asset/jeu.jpg';
+import ACMOTORS from '../asset/A-C-MOTORS.jpg';
+import raspi from '../asset/pi.jpg';
+
 
 export const projects: Project[] = [
   {
@@ -7,8 +10,8 @@ export const projects: Project[] = [
     title: 'AC-Motor',
     description:
       "Conception et modélisation d’une base de données relationnelle pour AC-Motors, avec MySQL pour la gestion des tables et l'optimisation des requêtes, afin de gérer efficacement produits, clients et commandes.",
-    image: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    tags: ['Mysql', 'mocodo', 'looping','SQLite'],
+    image: ACMOTORS,
+    tags: ['MySQl', 'mocodo', 'looping','SQLite'],
     demoUrl: 'https://example.com/demo1',
     codeUrl: 'https://github.com/example/project1',
   },
@@ -25,8 +28,7 @@ export const projects: Project[] = [
   {
     id: 'project-3',
     title: 'site web pour un escape game ',
-    description:
-      "Développement d’un site responsive en HTML/CSS, utilisant Flexbox pour une navigation fluide et une présentation optimale des services de l'agence , des services .",
+    description:"Développement d’un site responsive en HTML/CSS, utilisant Flexbox pour une navigation fluide et une présentation optimale des services de l'agence , des services .",
     image: "https://images.pexels.com/photos/7948060/pexels-photo-7948060.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     tags: ['html', 'css', 'Visual-code'],
     demoUrl: 'https://example.com/demo3',
@@ -35,9 +37,8 @@ export const projects: Project[] = [
   {
     id: 'project-4',
     title: 'Installation d’un poste de travail - Debian',
-    description:
-      "Installation et configuration d’un poste sur Raspberry Pi avec système d’exploitation, SQL, Python et packages adaptés, renforçant mes compétences en systèmes embarqués et outils techniques.",
-    image: "https://images.pexels.com/photos/3560044/pexels-photo-3560044.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:"Installation et configuration d’un poste sur Raspberry Pi avec système d’exploitation, SQL, Python et packages adaptés, renforçant mes compétences en systèmes embarqués et outils techniques.",
+    image:raspi,
     tags: ['linux', 'mariadb', 'sql', 'protocole ssh'],
     demoUrl: 'https://example.com/demo4',
     codeUrl: 'https://github.com/example/project4',
