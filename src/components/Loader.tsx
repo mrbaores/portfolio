be2334142cca796f4b64f3f19bb7e083503b7f16
Mohamed <moussabouremani@gmail.com>
2025-07-05 20:21:58 +0200

    ajout loader 2

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import logo from '../asset/logo.jpg';
 
-const Loader = () => (
-  <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-gray-900 z-50">
-    <img src={logo} alt="Logo" className="w-24 h-24 animate-spin" />
-  </div>
-);
+const Loader: React.FC = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-gray-900 z-50">
+      <div className="relative w-24 h-24">
+        {/* Cercle extérieur avec dégradé tournant */}
+        <div className="absolute inset-0 rounded-full border-4 border-t-indigo-500 border-r-transparent border-b-indigo-500 border-l-transparent animate-spin"></div>
+
+        {/* Cercle intérieur */}
+        <div className="absolute inset-4 rounded-full border-2 border-indigo-700 opacity-50"></div>
+
+        {/* Points tournants */}
+        <div className="absolute top-0 left-1/2 w-3 h-3 bg-indigo-600 rounded-full animate-spin origin-[50%_120%]"></div>
+        <div className="absolute bottom-0 left-1/2 w-3 h-3 bg-indigo-400 rounded-full animate-spin origin-[50%_-20%] animation-delay-1500"></div>
+
+        {/* Centre */}
+        <div className="absolute inset-10 rounded-full bg-indigo-900 opacity-80"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Loader;
